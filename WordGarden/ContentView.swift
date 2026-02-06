6120afd9485e2760f601b7a5c1ab488ae7ccdd09
Zimeng Yang <zimengyang8@gmail.com>
2026-02-05 23:20:55 -0500

    Initial Commit

---
+++ WordGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGarden
+//
+//  Created by Zimeng Yang on 2/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Zimeng Yang on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
